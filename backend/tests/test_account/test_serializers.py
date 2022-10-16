628fa9db34fc06e7d2bd8aefe2f3d2969de106a7
john <izolotavin99@gmail.com>
2022-10-16 16:52:11 +0300

    1) tests.test_account architecture added

---
+++ backend/tests/test_account/test_serializers.py
@@ -0,0 +1,38 @@
+####################################
+#    TEST's ProfileSerializer    #
+####################################
+
+class TestProfileSerializer:
+    pass
+
+
+####################################
+#    TEST's UserSerializer    #
+####################################
+
+class TestUserSerializer:
+    pass
+
+
+####################################
+#    TEST's UserDocumentSerializer    #
+####################################
+
+class TestUserDocumentSerializer:
+    pass
+
+
+####################################
+#    TEST's FuelCardSerializer    #
+####################################
+
+class TestFuelCardSerializer:
+    pass
+
+
+####################################
+#    TEST's     #
+####################################
+
+class TestNotificationSerializer:
+    pass
---
+++ backend/tests/test_account/test_validators.py
@@ -0,0 +1,25 @@
+
+####################################
+#    TEST's ProfileFIOValidator    #
+####################################
+
+class TestProfileFIOValidator:
+    pass
+
+########################################
+#    TEST's FuelCardNumberValidator    #
+########################################
+class TestFuelCardNumberValidator:
+    pass
+
+############################################
+#    TEST's UserPasswordRepeatValidator    #
+############################################
+class TestUserPasswordRepeatValidator:
+    pass
+
+######################################
+#    TEST's ProfilePhoneValidator    #
+######################################
+class TestProfilePhoneValidator:
+    pass
