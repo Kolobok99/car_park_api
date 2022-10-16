2bd66588308a0c46d761dac8dc00770e00a7c896
john <izolotavin99@gmail.com>
2022-10-16 16:55:32 +0300

    1) tests.test_api architecture added

---
+++ backend/tests/test_api/test_views.py
@@ -0,0 +1,70 @@
+##############################
+#    TEST's CarAPIViewSet    #
+##############################
+
+class TestCarAPIViewSet:
+    pass
+
+
+###################################
+#    TEST's CarBrandAPIViewSet    #
+###################################
+
+class TestCarBrandAPIViewSet:
+    pass
+
+
+###############################
+#    TEST's UserAPIViewSet    #
+###############################
+
+class TestUserAPIViewSet:
+    pass
+
+
+########################################
+#    TEST's DocumentTypesAPIViewSet    #
+########################################
+
+class TestDocumentTypesAPIViewSet:
+    pass
+
+
+######################################
+#    TEST's CarDocumentAPIViewSet    #
+######################################
+
+class TestCarDocumentAPIViewSet:
+    pass
+
+
+#######################################
+#    TEST's UserDocumentAPIViewSet    #
+#######################################
+
+class TestUserDocumentAPIViewSet:
+    pass
+
+
+#######################################
+#    TEST's RepairRequestAPIViewSet    #
+#######################################
+
+class TestRepairRequestAPIViewSet:
+    pass
+
+
+################################
+#    TEST's CardsAPIViewSet    #
+################################
+
+class TestCardsAPIViewSet:
+    pass
+
+
+#######################################
+#    TEST's NotificationAPIViewSet    #
+#######################################
+
+class TestNotificationAPIViewSet:
+    pass
